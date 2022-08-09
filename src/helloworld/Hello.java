ea1da246a38f8d469d9d1cd25d80601b3160e0d0
Sushil <sushilchaudhary9600@gmail.com>
2022-08-08 20:15:10 -0400

    initial commit

---
+++ src/helloworld/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author SUSHIL CHAUDHARY
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow world");
+
+	}
+
+}
